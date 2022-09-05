c4b5aecbb488570b4ab9466f5c20b97889d9045d
guy0017 <waesin@gmail.com>
2022-09-05 19:13:48 +0100

    add restriction so that only signed in users can view home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,6 +8,9 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: () => import("../views/HomeView.vue"),
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: "/signin",
@@ -19,6 +23,34 @@ const router = createRouter({
       component: () => import("../views/RegisterView.vue"),
     },
   ],
+});
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(), 
+      (user) => {
+        removeListener();
+        resolve(user);
+
+      },
+
+      reject
+    )
+  })
+}
+
+router.beforeEach(async(to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (await getCurrentUser()) {
+      next();
+    } else {
+      alert("You don't have access!");
+      next("/signin");
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
