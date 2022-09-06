921b4757dfdd2c5fe9beaf76cd9e3334c1c941c9
guy0017 <waesin@gmail.com>
2022-09-06 10:00:51 +0100

    add username field in register

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -5,8 +5,12 @@ import router from "@/router";
 
 const email = ref("");
 const password = ref("");
+const username = ref("")
+
 const register = () => {
   const auth = getAuth();
+  // console.log(username.value)
+  // email and username is accessed here so use API here in promise.all to send to backend
   createUserWithEmailAndPassword(auth, email.value, password.value)
     .then((data) => {
       router.push("/");
@@ -19,11 +23,12 @@ const register = () => {
 
 <template>
   <h1>Create an Account</h1>
+  <p><input type="text" placeholder="Username" v-model="username" /></p>
   <p><input type="text" placeholder="Email" v-model="email" /></p>
   <p><input type="password" placeholder="Password" v-model="password" /></p>
   <p><button @click="register">Submit</button></p>
 </template>
 
 <style scoped>
-  
+
 </style>
