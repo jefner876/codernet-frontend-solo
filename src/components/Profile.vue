7e46a4789d45f0a3291342a82b83b8d53fbfef39
guy0017 <waesin@gmail.com>
2022-09-08 10:20:44 +0100

    add profile page edit function. Need to refactor button status

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,36 +1,66 @@
 <script setup lang="ts">
-    import {ref} from 'vue'
+import { ref, reactive } from "vue";
 
-const bool = false;
+let DOB = ref("");
+let location = ref("");
+let bio = ref("");
 
-const profile = ref({
-  username: 'Hello',
-  email: 'Hello',
+let buttonStatus = reactive({ edit: true });
+
+const profile = reactive({
+  username: "Hello",
+  email: "Hello@hello.com",
   DOB: 0,
-  location: 'Manchester',
-  avatar: "https://gravatar.com/avatar/494331cd1db71a9c5928845bc556782b?s=400&d=robohash&r=x",
-  bio: 'Coding....'
+  location: "Manchester",
+  avatar:
+    "https://gravatar.com/avatar/494331cd1db71a9c5928845bc556782b?s=400&d=robohash&r=x",
+  bio: "Coding....",
 });
 
+const handleClick = (event: any) => {
+  event.preventDefault();
 
+  buttonStatus.edit = !buttonStatus.edit;
+
+  profile.DOB = parseInt(DOB.value);
+  profile.location = location.value;
+  profile.bio = bio.value;
+};
 </script>
 
 <template>
-
-    <h2>Username: {{profile.username}}</h2>
-    <img  :src="profile.avatar"/>
-    <p>Email: {{profile.email}}</p>
-    <p>Age: {{profile.DOB}}</p>
-    <p>Location: {{profile.location}}</p>
-    <p>About me: {{profile.bio}}</p>
-  
-
+  <form>
+    <h2>Username: {{ profile.username }}</h2>
+    <img :src="profile.avatar" />
+    <p>Email: {{ profile.email }}</p>
+    <p>Age: {{ profile.DOB }}</p>
+    <input
+      type="text"
+      placeholder="DOB"
+      v-model="DOB"
+      v-if="buttonStatus.edit"
+    />
+    <p>Location: {{ profile.location }}</p>
+    <input
+      type="text"
+      placeholder="location"
+      v-model="location"
+      v-if="buttonStatus.edit"
+    />
+    <p>About me: {{ profile.bio }}</p>
+    <input
+      type="text"
+      placeholder="bio"
+      v-model="bio"
+      v-if="buttonStatus.edit"
+    />
+    <button @click="handleClick">Edit</button>
+  </form>
 </template>
 
 <style scoped>
-    img {
-        width: 200px;
-        height: 200px;
-    }
-
+img {
+  width: 200px;
+  height: 200px;
+}
 </style>
